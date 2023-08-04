445d4a5e78be7004effe5d7101abb9f03ffff92e
azam-rs <azam@rapidsilicon.com>
2023-08-04 12:25:32 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/Azam-RS/RGPT/tree/main/src/docs",
+            "https://github.com/Azam-RS/RGPT/tree/main/src",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/Azam-RS/RGPT/tree/main/src/docs",
+            "https://github.com/Azam-RS/RGPT/tree/main/src",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
