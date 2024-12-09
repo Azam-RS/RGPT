001d9ac00a396d9a0d1050c406bbe2698f19791a
ShoaibGhori-Primis-AI <shoaib@primis.ai>
2024-12-09 19:07:16 +0500

    fix: sequence

--- src/pages/papers/index.js
+++ src/pages/papers/index.js
@@ -28,25 +28,6 @@ export default function Papers() {
                 <h1>Our Papers</h1>
               </div>
             </div>
-            <div className="col col--12">
-              <div className="desc-area">
-                <div className="paper-title">
-                  <h1>AIVRIL: AI-DRIVEN RTL GENERATION WITH VERIFICATION IN-THE-LOOP</h1>
-                  <p>Mubashir ul Islam¹, Humza Sami¹, Pierre-Emmanuel Gaillardon¹˒², and Valerio Tenace¹</p>
-                  <p>¹PrimisAI, Los Gatos, CA, USA</p>
-                  <p>²University of Utah, Salt Lake City, UT, USA</p>
-                  <div className="conf-bg"></div>
-                </div>
-                <div className="paper-desc">
-                  <h4>Abstract:</h4>
-                  <p>Large Language Models (LLMs) are computational models capable of performing complex natural language processing tasks. Leveraging these capabilities, LLMs hold the potential to transform the entire hardware design stack, with predictions suggesting that front-end and back-end tasks could be fully automated in the near future. Currently, LLMs show great promise in streamlining Register Transfer Level (RTL) generation, enhancing efficiency, and accelerating innovation. However, their probabilistic nature makes them prone to inaccuracies—a significant drawback in RTL design, where reliability and precision are essential.</p>
-                  <p>To address these challenges, this paper introduces AIVRIL, an advanced framework designed to enhance the accuracy and reliability of RTL-aware LLMs. AIVRIL employs a multi-agent, LLM- agnostic system for automatic syntax correction and functional verification, significantly reduc- ing—and in many cases, completely eliminating—instances of erroneous code generation. Experi- mental results conducted on the VerilogEval-Human dataset show that our framework improves code quality by nearly 2× when compared to previous works, while achieving an 88.46% success rate in meeting verification objectives. This represents a critical step toward automating and optimizing hardware design workflows, offering a more dependable methodology for AI-driven RTL design.</p>
-                </div>
-                <div className="dwn-btn">
-                  <button className="btn-more" title="Read More" onClick={handleRedirect}>Read More</button>
-                </div>
-              </div>
-            </div>
 
             <div className="col col--12">
               <div className="desc-area">
@@ -68,6 +49,27 @@ export default function Papers() {
                 </div>
               </div>
             </div>
+            <div className="col col--12">
+              <div className="desc-area">
+                <div className="paper-title">
+                  <h1>AIVRIL: AI-DRIVEN RTL GENERATION WITH VERIFICATION IN-THE-LOOP</h1>
+                  <p>Mubashir ul Islam¹, Humza Sami¹, Pierre-Emmanuel Gaillardon¹˒², and Valerio Tenace¹</p>
+                  <p>¹PrimisAI, Los Gatos, CA, USA</p>
+                  <p>²University of Utah, Salt Lake City, UT, USA</p>
+                  <div className="conf-bg"></div>
+                </div>
+                <div className="paper-desc">
+                  <h4>Abstract:</h4>
+                  <p>Large Language Models (LLMs) are computational models capable of performing complex natural language processing tasks. Leveraging these capabilities, LLMs hold the potential to transform the entire hardware design stack, with predictions suggesting that front-end and back-end tasks could be fully automated in the near future. Currently, LLMs show great promise in streamlining Register Transfer Level (RTL) generation, enhancing efficiency, and accelerating innovation. However, their probabilistic nature makes them prone to inaccuracies—a significant drawback in RTL design, where reliability and precision are essential.</p>
+                  <p>To address these challenges, this paper introduces AIVRIL, an advanced framework designed to enhance the accuracy and reliability of RTL-aware LLMs. AIVRIL employs a multi-agent, LLM- agnostic system for automatic syntax correction and functional verification, significantly reduc- ing—and in many cases, completely eliminating—instances of erroneous code generation. Experi- mental results conducted on the VerilogEval-Human dataset show that our framework improves code quality by nearly 2× when compared to previous works, while achieving an 88.46% success rate in meeting verification objectives. This represents a critical step toward automating and optimizing hardware design workflows, offering a more dependable methodology for AI-driven RTL design.</p>
+                </div>
+                <div className="dwn-btn">
+                  <button className="btn-more" title="Read More" onClick={handleRedirect}>Read More</button>
+                </div>
+              </div>
+            </div>
+
+            
           </div>
         </div>
       </div>
